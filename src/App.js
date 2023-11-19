faad2e17447dbc21f817366c9a08c08cffdaf1d1
osh07 <hsg0709@naver.com>
2023-11-19 20:38:14 +0900

    tictactoe app clone coding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import "./App.css"
+import Board from "./components/Board";
+
+/*import "./App.css"
 import Background from "./components/Background";
 
 function App() {
@@ -8,5 +10,17 @@ function App() {
     </div>
   );
 }
-
+*/
+function App(){
+  return(
+    <div className="game">
+        <div className="game-board">
+          <Board/>
+        </div>
+        <div className="game-info">
+          game-info
+        </div>
+    </div>
+  );
+}
 export default App;
