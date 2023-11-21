de21460462365bc3601e3f90078e030c11245682
osh07 <hsg0709@naver.com>
2023-11-21 16:40:40 +0900

    tictactoe app clone coding

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,47 +1,75 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Square from './Square'
 import "./Board.css";
 
-export default class Board extends Component {
-
-    constructor(props){
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null)
+const Board = () => {
+     
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+    const calculateWinner = (squares) => {
+        const lines = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+        for (let index = 0; index < lines.length; index++) {
+            const [a, b, c] = lines[index];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
         }
+        return null;
+    }
+    const winner = calculateWinner(squares);
+    let status;
+    if(winner){
+        status='Winner:' + winner;
+    }else {
+        status=`Next player ${xIsNext? 'X': 'O'}`;
     }
 
-    handleClick(i){
-        const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({ squeres: squares });
+    const handleClick = (i) => {
+        const newSquares = squares.slice();
+
+        if (calculateWinner(newSquares) || (newSquares[i])) {
+            return;
+        }
+        newSquares[i] = xIsNext ? 'X' : 'O';
+        setSquares(newSquares);
+        setXIsNext(prev => !prev)
     }
 
-    renderSquare(i){
-    return <Square value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)} />
+    const renderSquare = (i) =>{
+    return <Square value={squares[i]}
+        onClick={() => handleClick(i)} />
     }
 
-    render() {
-        return (
-        <div>
-            <div className='status'>Next Player: X ,O</div>
-            <div className='board-row'>
-                {this.renderSquare(0)}
-                {this.renderSquare(1)}
-                {this.renderSquare(2)}
-            </div>
-            <div className='board-row'>
-                {this.renderSquare(3)}
-                {this.renderSquare(4)}
-                {this.renderSquare(5)}
-            </div>
-            <div className='board-row'>
-                {this.renderSquare(6)}
-                {this.renderSquare(7)}
-                {this.renderSquare(8)}
-            </div>
+    return (
+    <div>
+        <div className='status'>{status}</div>
+        <div className='board-row'>
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
         </div>
-        )
-    }
+        <div className='board-row'>
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+        </div>
+        <div className='board-row'>
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+        </div>
+    </div>
+    )
 }
+
+
+export default Board
